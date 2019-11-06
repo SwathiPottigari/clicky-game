5b2151b7b21dda318c10e91b927c88818817d125
Swathi Pottigari <pottigariswathi@gmail.com>
2019-11-05 21:06:29 -0800

    Added the components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import friends from '../src/friends.json';
+import Wrapper from "../src/components/Wrapper/Wrapper.js";
+import FriendsCard from "../src/components/FriendsCard/FriendsCard.js";
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+class App extends Component {
+  state = {
+    friends
+  }
+
+  render() {
+    return (<Wrapper>
+      <h4>Clicky Game</h4>
+      <FriendsCard/>
+    </Wrapper>);
+  }
 }
 
 export default App;
---
+++ src/components/FriendsCard/FriendsCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FriendsCard(props){
+return(<div>Hello Friends</div>);
+}
+
+export default FriendsCard;
