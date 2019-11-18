f0a2c7deaadabd8c2906a2b6eadca30130ba923e
Swathi Pottigari <pottigariswathi@gmail.com>
2019-11-18 10:33:22 -0800

    Updates

---
+++ precache-manifest.3d26373b470372aa81ae9fb2f8c7153d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8b474a7ab9921b85adce6f079447b5",
+    "url": "/SwathiPottigari/clicky-game/index.html"
+  },
+  {
+    "revision": "b8182b23a0a797a04167",
+    "url": "/SwathiPottigari/clicky-game/static/css/main.00c3a30c.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/SwathiPottigari/clicky-game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "b8182b23a0a797a04167",
+    "url": "/SwathiPottigari/clicky-game/static/js/main.1c4634de.chunk.js"
+  },
+  {
+    "revision": "a4ab00311067cf03395d",
+    "url": "/SwathiPottigari/clicky-game/static/js/runtime-main.29879569.js"
+  }
+]);
